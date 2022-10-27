5f5c30c0343a1c32befb6a12da6af2124acb29b6
SarahQianhuaAn <anqianhua28@126.com>
2022-10-26 21:03:03 -0700

    multiple routes

--- index.js
+++ index.js
@@ -1,72 +1,68 @@
-// const prevButton = document.getElementById("prev");
-// const nextButton = document.getElementById("next");
-const myForm = document.getElementById("myForm");
-const reader = new FileReader();
-let waypointsOfRoutes = [];
+let data;
 
-myForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const [myCSV] = document.getElementById("csvFile").files;
-  if (myCSV) {
-    reader.readAsText(myCSV);
-  }
-});
-
-reader.addEventListener(
-  "load",
-  () => {
-    // console.log(reader.result)
-    const data = reader.result;
-    getData(data).catch((error) => {
-      console.error(error);
-    });
-  },
-  false
-);
-
-async function getData(data) {
-  // const response = await fetch("test.csv");
+async function getWaypoints() {
+  // const response = await fetch("routes.csv");
   // const data = await response.text();
   const rows = data.split(/\n|\r\n/).slice(1);
+  var currentRouteIndex = "";
+  var addresses = [];
+  let waypoints = [];
   rows.forEach((row) => {
     const columns = row.split(",");
     const routeIndex = columns[0];
-    const stopIndex = columns[1];
     const geocodedAddress = columns[3];
     if (routeIndex != "DISMISSED REQUEST") {
-      waypointsOfRoutes.push([routeIndex, stopIndex, geocodedAddress]);
+      if (routeIndex != currentRouteIndex) {
+        if (addresses.length > 0) {
+          waypoints.push(addresses);
+        }
+        currentRouteIndex = routeIndex;
+        addresses = [];
+      }
+      addresses.push({ location: geocodedAddress, stopover: true });
     }
   });
-}
-
-function getWaypoints() {
-  let waypoints = [];
-  waypointsOfRoutes.forEach((waypoint) => {
-    let address = waypoint[2];
-    waypoints.push({ location: address });
-  });
-  console.log(waypoints);
+  if (addresses.length > 0) {
+    waypoints.push(addresses);
+  }
+  // console.log(waypoints);
   return waypoints;
 }
 
-function calcRoute(directionsService, directionsRenderer) {
-  var depot = { lat: 33.7175, lng: -117.8311 };
+async function calcRoute(directionsService, directionsRenderer) {
+  var depot = document.getElementById("depot").value;
+  var index = parseInt(document.getElementById("index").value);
+  let waypointsOfRoutes = await getWaypoints();
+
   const directionsRequest = {
     origin: depot,
     destination: depot,
-    waypoints: getWaypoints(),
+    waypoints: waypointsOfRoutes[index - 1],
     travelMode: "DRIVING",
   };
+
   directionsService.route(directionsRequest, (result, status) => {
     if (status == google.maps.DirectionsStatus.OK) {
       directionsRenderer.setDirections(result);
+      console.log(directionsRequest);
     } else {
       console.error("Direction request failed.");
     }
   });
 }
 
+function newAutocomplete(id) {
+  new google.maps.places.Autocomplete(document.getElementById(id));
+}
+
+function initAutocomplete() {
+  newAutocomplete("depot");
+}
+
 function initMap() {
+  const csvFile = document.getElementById("csvFile");
+  const reader = new FileReader();
+
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 9,
     center: { lat: 33.7175, lng: -117.8311 },
@@ -74,13 +70,37 @@ function initMap() {
 
   const directionsService = new google.maps.DirectionsService();
   const directionsRenderer = new google.maps.DirectionsRenderer();
+
   directionsRenderer.setMap(map);
   directionsRenderer.setPanel(document.getElementById("directionsPanel"));
-  calcRoute(directionsService, directionsRenderer);
-  var goButton = document.getElementById("go");
-  goButton.addEventListener("click", () =>
-    calcRoute(directionsService, directionsRenderer)
+
+  initAutocomplete();
+
+  // const prevButton = document.getElementById("prev");
+  // const nextButton = document.getElementById("next");
+
+  csvFile.addEventListener("change", (e) => {
+    e.preventDefault();
+    const [myCSV] = document.getElementById("csvFile").files;
+    if (myCSV) {
+      reader.readAsText(myCSV);
+    }
+  });
+
+  reader.addEventListener(
+    "load",
+    () => {
+      // console.log(reader.result)
+      data = reader.result;
+    },
+    false
   );
+
+  var goButton = document.getElementById("go");
+
+  goButton.addEventListener("click", () => {
+    calcRoute(directionsService, directionsRenderer);
+  });
 }
 
 window.initMap = initMap;
